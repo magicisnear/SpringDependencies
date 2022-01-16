328270f622e37f2e15879ee7be614e5078d52219
funre <magicisnear@bk.ru>
2022-01-16 14:09:34 +0300

    save

--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -1,8 +1,11 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class Egg6 {
 
     private Needle7 needle;
+
 
     public Egg6(Needle7 needle) {
         this.needle = needle;
